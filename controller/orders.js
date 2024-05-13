d3d61dffa12df59d12b1f7767517803d52e4e010
MAHafizh <hafizh12jr@gmail.com>
2024-05-14 04:16:13 +0700

    add new config

--- controller/orders.js
+++ controller/orders.js
@@ -2,10 +2,18 @@ import { Users, Orders } from "../models/userModel.js";
 import jwt from "jsonwebtoken";
 
 export const addOrder = async (req, res) => {
-  const { user_id, formal_shirt, shirt, outer, jeans, pants, underwear } = req.body;
+  const { user_id, formal_shirt, shirt, outer, jeans, pants, underwear, pickup_time, service_type } = req.body;
 
   try {
-    const orderIdPrefix = Math.random().toString(36).substring(2, 5).toUpperCase();
+    const generateRandomDigits = (length) => {
+      let result = '';
+      for (let i = 0; i < length; i++) {
+          result += Math.floor(Math.random() * 10); // Generate random digit from 0 to 9
+      }
+      return result;
+  };
+
+    const orderIdPrefix = generateRandomDigits(3);
     const ordersCount = await Orders.count();
     const orderNumber = String(ordersCount + 1).padStart(5, "0");
     const order_id = `${orderIdPrefix}${orderNumber}`;
@@ -19,6 +27,8 @@ export const addOrder = async (req, res) => {
         jeans,
         pants,
         underwear,
+        pickup_time,
+        service_type
       });
 
     res.status(201).json({ msg: 'Order added successfully', order_id: newOrder.order_id });
